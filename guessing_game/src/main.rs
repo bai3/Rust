a8731f15ce86cd79c58e5c73cd468c6db5b94f06
bai3 <bzxdggyx@gmail.com>
2022-02-14 18:35:36 +0800

    feat: 继续学习

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,5 +1,28 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
-  println!
+  println!("Guess the number!");
+  println!("Please input your guess.");
+  // mut 可变变量
+  let mut guess = String::new();
+  let secret_number = rand::thread_rng().gen_range(1..10);
+  // println!("The secret number is {}", secret_number);
+  // let a = 33;
+  loop {
+    io::stdin()
+    .read_line(&mut guess)
+    .expect("Failed to read input");
+    println!("You guess: {}", guess);
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+    match guess.cmp(&secret_number) {
+      Ordering::Less => println!("Too small"),
+      Ordering::Greater => println!("Too big"),
+      Ordering::Equal => {
+        println!("You win");
+        break;
+      }
+    }
+  }
 }
